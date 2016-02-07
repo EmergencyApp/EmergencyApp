28b8a26d0212c39b990016c7544401ae3d0a8dea
talisk <talisk@foxmail.com>
2016-02-07 16:25:24 +0800

    New File.

---
+++ Emergency/WarningVC/WarningViewController.h
@@ -0,0 +1,13 @@
+//
+//  WarningViewController.h
+//  Emergency
+//
+//  Created by 孙恺 on 16/2/7.
+//  Copyright © 2016年 sunkai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WarningViewController : UIViewController
+
+@end
