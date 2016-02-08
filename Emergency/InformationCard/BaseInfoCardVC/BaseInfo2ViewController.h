d4902113661669489c0cc88200dd0788b438c43e
talisk <talisk@foxmail.com>
2016-02-08 23:31:29 +0800

    Add second step of base information.

---
+++ Emergency/InformationCard/BaseInfoCardVC/BaseInfo2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  BaseInfo2ViewController.h
+//  Emergency
+//
+//  Created by 孙恺 on 16/2/8.
+//  Copyright © 2016年 sunkai. All rights reserved.
+//
+
+#import "BasicCardViewController.h"
+
+@interface BaseInfo2ViewController : BasicCardViewController
+
+@end
---
+++ Emergency/InformationCard/BaseInfoCardVC/BaseInfo2ViewController.m
@@ -0,0 +1,37 @@
+//
+//  BaseInfo2ViewController.m
+//  Emergency
+//
+//  Created by 孙恺 on 16/2/8.
+//  Copyright © 2016年 sunkai. All rights reserved.
+//
+
+#import "BaseInfo2ViewController.h"
+
+@interface BaseInfo2ViewController ()
+
+@end
+
+@implementation BaseInfo2ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
