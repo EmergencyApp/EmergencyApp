2755897c646cafccc73d1de2551435cc9647e534
talisk <talisk@foxmail.com>
2016-02-13 21:56:40 +0800

    Add EmergencyContact Card.

--- Emergency/WarningVC/WarningViewController.m
+++ Emergency/WarningVC/WarningViewController.m
@@ -14,11 +14,14 @@
 #import <Colours/Colours.h>
 #import "HMFileManager.h"
 #import "SKAudioManager.h"
+#import "CoreMediaFuncManagerVC.h"
 
 @interface WarningViewController () <ZLSwipeableViewDataSource, ZLSwipeableViewDelegate, UITableViewDataSource, UITableViewDelegate>
 
 @property (nonatomic, strong) ZLSwipeableView *cardsStackView;
 @property (nonatomic, strong) NSArray *titleArray;
+
+@property (nonatomic, strong) NSArray *contactsArray;
 
 @end
 
@@ -34,6 +37,11 @@
     
     AVAudioPlayer *player = [[SKAudioManager shareManager] playingMusic:@"warning_audio"];
     
+    self.contactsArray = [HMFileManager getObjectByFileName:@"contactsArray"];
+    
+    if (!self.contactsArray || self.contactsArray.count==0) {
+        self.contactsArray = [[NSMutableArray alloc] init];
+    }
     
     self.titleArray = @[@[@"姓名", @"出生日期", @"年龄", @"生理性别", @"血型"],
                         @[@"身高", @"体重", @"腰围", @"BMI"],
@@ -41,7 +49,7 @@
                         @[@"进餐", @"梳洗", @"穿衣", @"如厕", @"活动能力"]
                         ];
     
-    _tableViewSN = 1;
+    _tableViewSN = 0;
     
     UIImageView *imageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
     imageView.image = [UIImage imageNamed:@"bt_mymusic_time_bg_afternoon.jpg"];
@@ -115,10 +123,16 @@
     BasicCardView *baseinfocard;
     
     if (_tableViewSN == 5) {
-        _tableViewSN -= 4;
+        _tableViewSN -= 5;
     }
     
     switch (_tableViewSN) {
+        case 0:{
+            baseinfocard = [[BasicCardView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width/5*4, 44*(self.contactsArray.count)?self.contactsArray.count:1+68)];
+            [baseinfocard.tableView setUserInteractionEnabled:YES];
+            [baseinfocard.tableView setScrollEnabled:NO];
+            break;
+        }
         case 1:
             baseinfocard = [[BasicCardView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width/5*4, 44*5+68)];
             break;
@@ -137,8 +151,13 @@
     
     [baseinfocard.tableView setTag:_tableViewSN++];
     
-    [baseinfocard.tableView setBackgroundColor:[UIColor black50PercentColor]];
-    [baseinfocard.tableView setSeparatorColor:[UIColor whiteColor]];
+    if (!baseinfocard.tableView.tag) {
+        [baseinfocard.tableView setBackgroundColor:[UIColor whiteColor]];
+        [baseinfocard.tableView setSeparatorColor:[UIColor black75PercentColor]];
+    } else {
+        [baseinfocard.tableView setBackgroundColor:[UIColor black50PercentColor]];
+        [baseinfocard.tableView setSeparatorColor:[UIColor whiteColor]];
+    }
     
     [baseinfocard.tableView setDelegate:self];
     [baseinfocard.tableView setDataSource:self];
@@ -157,42 +176,91 @@
 
 #pragma mark - Card TableView
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+    [[tableView cellForRowAtIndexPath:indexPath] setSelected:NO];
+    
+    if (self.contactsArray.count) {
+        [CoreMediaFuncManagerVC call:self.contactsArray[indexPath.row][@"phoneNO"] inViewController:self failBlock:^{
+            NSLog(@"不能打");
+        }];
+    }
+    
+}
+
 - (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
     return 44.0f;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    NSLog(@"%i",tableView.tag);
     
-    static NSString *cellID = @"warningCell";
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
-    if (!cell) {
-        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:cellID];
+    
+    if (!tableView.tag) {
+        
+        if (self.contactsArray.count) {
+            static NSString *cellID = @"contacts";
+            UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+            if (!cell) {
+                cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:cellID];
+            }
+            
+            [cell setBackgroundColor:[UIColor whiteColor]];
+            [cell.textLabel setTextColor:[UIColor blackColor]];
+            [cell.detailTextLabel setTextColor:[UIColor blueColor]];
+            
+            NSString *name = [NSString stringWithFormat:@"%@%@", self.contactsArray[indexPath.row][@"lastname"], self.contactsArray[indexPath.row][@"firstname"]];
+            [cell.textLabel setText:name];
+            [cell.detailTextLabel setText:self.contactsArray[indexPath.row][@"phoneNO"]];
+            
+            return cell;
+        } else {
+            static NSString *cellID = @"empty";
+            UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+            if (!cell) {
+                cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+            }
+            
+            [cell setBackgroundColor:[UIColor whiteColor]];
+            [cell.textLabel setTextColor:[UIColor blackColor]];
+            [cell.detailTextLabel setTextColor:[UIColor blueColor]];
+            
+            [cell.textLabel setText:@"未设置紧急联系人"];
+            
+            return cell;
+        }
+        
+    } else {
+        static NSString *cellID = @"warningCell";
+        UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:cellID];
+        if (!cell) {
+            cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:cellID];
+        }
+        
+        [cell setBackgroundColor:[UIColor black50PercentColor]];
+        [cell.textLabel setTextColor:[UIColor whiteColor]];
+        [cell.detailTextLabel setTextColor:[UIColor whiteColor]];
+        
+        //    [cell.textLabel setText:@"title"];
+        //    [cell.detailTextLabel setText:@"detail"];
+        
+        NSArray *savedData = (NSArray<NSArray *> *)[HMFileManager getObjectByFileName:@"baseInfoArray"];
+        
+        [cell.textLabel setText:self.titleArray[tableView.tag-1][indexPath.row]];
+        [cell.detailTextLabel setText:savedData[tableView.tag-1][indexPath.row]];
+        
+        return cell;
+
     }
-    
-    [cell setBackgroundColor:[UIColor black50PercentColor]];
-    [cell.textLabel setTextColor:[UIColor whiteColor]];
-    [cell.detailTextLabel setTextColor:[UIColor whiteColor]];
-    
-//    [cell.textLabel setText:@"title"];
-//    [cell.detailTextLabel setText:@"detail"];
-    
-    NSArray *savedData = (NSArray<NSArray *> *)[HMFileManager getObjectByFileName:@"baseInfoArray"];
-    
-    [cell.textLabel setText:self.titleArray[tableView.tag-1][indexPath.row]];
-    [cell.detailTextLabel setText:savedData[tableView.tag-1][indexPath.row]];
-    
-    return cell;
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-
-    
     return 1;
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     switch (tableView.tag) {
+        case 0:
+            return self.contactsArray.count?self.contactsArray.count:1;
+            break;
         case 1:
             return 5;
             break;
